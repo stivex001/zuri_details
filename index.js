7677e83d66f4b2fb0532dae7fe4625178843b8b6
stivex001 <stephenadeyemo@gmail.com>
2022-06-21 16:08:59 +0100

    basic javascript program

---
+++ index.js
@@ -0,0 +1,7 @@
+const name = "Adeyemo Stephen";
+const height = "5.9"
+const country = "Nigeria";
+
+console.log(name)                // Adeyemo Stephen
+console.log(height + " feet")     // 5'8 feet
+console.log(country)             // Nigeria
